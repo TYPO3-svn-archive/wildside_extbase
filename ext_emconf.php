f58584d73108b594fe35392f50fb96c0c4d6df1b
claus@wildside.dk <claus@wildside.dk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-26 19:04:41 +0000

    Fixed minor naming annoyance.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wildside_extbase/trunk@42673 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "wildside_extbase".
 #
-# Auto generated 26-01-2011 17:07
+# Auto generated 26-01-2011 19:03
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,8 +11,8 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Wildside ExtBase Helpers',
-	'description' => 'Provides helpers etc. for ExtBase extensions, custom to Wildside A/S',
+	'title' => 'Wildside Extbase Helpers',
+	'description' => 'Provides helpers etc. for Extbase extensions, custom to Wildside A/S',
 	'category' => 'plugin',
 	'author' => 'Claus Due',
 	'author_email' => 'claus@wildside.dk',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"a0fd";s:14:"ext_tables.php";s:4:"8116";s:14:"ext_tables.sql";s:4:"d41d";s:16:"kickstarter.json";s:4:"fba3";s:49:"Classes/ViewHelpers/CheckboxCheckedViewHelper.php";s:4:"bd04";s:44:"Classes/ViewHelpers/DatePickerViewHelper.php";s:4:"58b9";s:38:"Classes/ViewHelpers/DateViewHelper.php";s:4:"cd7b";s:41:"Classes/ViewHelpers/ExplodeViewHelper.php";s:4:"271d";s:44:"Classes/ViewHelpers/FirstChildViewHelper.php";s:4:"6a67";s:41:"Classes/ViewHelpers/ImplodeViewHelper.php";s:4:"72dd";s:40:"Classes/ViewHelpers/InjectViewHelper.php";s:4:"e1b8";s:40:"Classes/ViewHelpers/NumberViewHelper.php";s:4:"8ca0";s:48:"Classes/ViewHelpers/SelectSelectedViewHelper.php";s:4:"4b92";s:37:"Classes/ViewHelpers/TcaViewHelper.php";s:4:"470d";s:48:"Classes/ViewHelpers/Format/UcfirstViewHelper.php";s:4:"53d2";s:44:"Classes/ViewHelpers/Inject/CssViewHelper.php";s:4:"8f58";s:43:"Classes/ViewHelpers/Inject/JsViewHelper.php";s:4:"6930";s:34:"Configuration/TypoScript/setup.txt";s:4:"012c";s:40:"Resources/Private/Language/locallang.xml";s:4:"ba6e";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"2a8e";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";}',
+	'_md5_values_when_last_written' => 'a:24:{s:6:"README";s:4:"64fa";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"a0fd";s:14:"ext_tables.php";s:4:"8116";s:14:"ext_tables.sql";s:4:"d41d";s:16:"kickstarter.json";s:4:"fba3";s:49:"Classes/ViewHelpers/CheckboxCheckedViewHelper.php";s:4:"bd04";s:44:"Classes/ViewHelpers/DatePickerViewHelper.php";s:4:"58b9";s:38:"Classes/ViewHelpers/DateViewHelper.php";s:4:"cd7b";s:41:"Classes/ViewHelpers/ExplodeViewHelper.php";s:4:"271d";s:44:"Classes/ViewHelpers/FirstChildViewHelper.php";s:4:"6a67";s:41:"Classes/ViewHelpers/ImplodeViewHelper.php";s:4:"72dd";s:40:"Classes/ViewHelpers/InjectViewHelper.php";s:4:"e1b8";s:40:"Classes/ViewHelpers/NumberViewHelper.php";s:4:"8ca0";s:48:"Classes/ViewHelpers/SelectSelectedViewHelper.php";s:4:"4b92";s:37:"Classes/ViewHelpers/TcaViewHelper.php";s:4:"470d";s:48:"Classes/ViewHelpers/Format/UcfirstViewHelper.php";s:4:"53d2";s:44:"Classes/ViewHelpers/Inject/CssViewHelper.php";s:4:"8f58";s:43:"Classes/ViewHelpers/Inject/JsViewHelper.php";s:4:"6930";s:34:"Configuration/TypoScript/setup.txt";s:4:"012c";s:48:"Resources/Patch/class.t3lib_mail_mailer.php.diff";s:4:"45e2";s:40:"Resources/Private/Language/locallang.xml";s:4:"ba6e";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"2a8e";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";}',
 );
 
 ?>
