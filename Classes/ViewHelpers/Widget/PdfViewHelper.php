bfcf636348472d1d43b7d8c873fd1ac04cea4905
claus@wildside.dk <claus@wildside.dk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-06 19:44:29 +0000

    Updated manual. Added option for custom path to wkhtmltopdf binary
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wildside_extbase/trunk@46304 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/Widget/PdfViewHelper.php
+++ Classes/ViewHelpers/Widget/PdfViewHelper.php
@@ -21,13 +21,19 @@ class Tx_WildsideExtbase_ViewHelpers_Widget_PdfViewHelper extends Tx_WildsideExt
 	 * @param string $filename Optional filename of downloaded PDF. Default is print.pdf
 	 * @return string
 	 */
-	public function render($typeNum=48151623420, $extension='tx_wildsideextbase_pdf', $stylesheet=NULL, $filename='print.pdf') {
+	public function render(
+			$typeNum=48151623420, 
+			$extension='tx_wildsideextbase_pdf', 
+			$stylesheet=NULL, 
+			$filename='print.pdf',
+			$wkhtmltopdf=NULL) {
 		$uniqId = uniqid('wsRenderPageAsPdf');
 		$code = <<< CODE
 <form action='?type={$typeNum}' method='post' id='{$uniqId}' style='display: none'>
 <input type='hidden' name='{$extension}[html]' value='' />
 <input type='hidden' name='{$extension}[stylesheet]' value='{$stylesheet}' />
 <input type='hidden' name='{$extension}[filename]' value='{$filename}' />
+<input type='hidden' name='{$extension}[wkhtmltopdf]' value='{$wkhtmltopdf}' />
 </form>
 CODE;
 		$script = <<< SCRIPT
